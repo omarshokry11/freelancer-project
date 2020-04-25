dca278bdf2192f8c75f4782cf3d016256e951d37
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-04-25 18:11:28 +0200

    Create Structure

---
+++ src/Portal.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Portal extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <h1>Portal</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Portal;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
+//global css
+// global css
+import "normalize.css";
+import "bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-const App = () => {
-    return <div>App</div>
-}
+import Portal from "./Portal.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Portal />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
